fbf6db8405332a9c6325e46737ad587e2314559c
Zac Brannelly <101668878@student.swin.edu.au>
2018-09-23 17:25:43 +1000

    WIP Primitive Cube entity (for the creation menu)

---
+++ ZEngine-Core/Map/Objects/Primitives/Cube.cpp
@@ -0,0 +1,19 @@
+#include "Cube.h"
+
+#include "../../../Component/MeshRenderer.h"
+#include "../../../Rendering/Mesh.h"
+#include "../../../Rendering/MeshFactory.h"
+#include "../../../Rendering/Material.h"
+#include "../../../Rendering/Shader.h"
+#include "../../../Misc/Factory.h"
+
+ZObject* Cube::CreateInstance(std::string name, ObjectType type)
+{
+	auto newEntity = new Entity(name);
+
+	auto cubeMesh = MeshFactory::CreateCube(name + "_mesh");
+	//auto shader = Factory::CreateInstance<Shader>(name + "_shader", ObjectType::SHADER);
+
+	return newEntity;
+}
+
---
+++ ZEngine-Core/Map/Objects/Primitives/Cube.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Entity.h"
+
+class Cube
+{
+	static ZObject* CreateInstance(std::string name, ObjectType type);
+};
+
