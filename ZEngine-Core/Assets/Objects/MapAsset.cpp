2fe212159ccfecfbb1914d767e561891e240ae4c
Zac Brannelly <101668878@student.swin.edu.au>
2018-10-07 20:16:49 +1100

    WIP - Map loading

--- ZEngine-Core/Assets/Objects/MapAsset.cpp
+++ ZEngine-Core/Assets/Objects/MapAsset.cpp
@@ -1,11 +1,32 @@
 #include "MapAsset.h"
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+using namespace nlohmann;
 
-
-MapAsset::MapAsset()
+MapAsset::MapAsset(string name) : Asset(name, ObjectType::MAP_ASSET)
 {
 }
 
+bool MapAsset::Load(string path)
+{
+	ifstream in(path, ios::in);
+
+	if (!in.is_open())
+	{
+		cout << "MAP_ASSET: Failed to load file: " << path << endl;
+		return false;
+	}
+
+
+	return true;
+}
+
+Map* MapAsset::GetMap() const
+{
+	return _map;
+}
 
 MapAsset::~MapAsset()
 {
--- ZEngine-Core/Assets/Objects/MapAsset.h
+++ ZEngine-Core/Assets/Objects/MapAsset.h
@@ -1,8 +1,21 @@
 #pragma once
-class MapAsset
+
+#include "../Asset.h"
+#include "../../../json/json.hpp"
+
+class Map;
+
+class MapAsset : public Asset
 {
 public:
-	MapAsset();
+	MapAsset(std::string name);
 	~MapAsset();
+
+	bool Load(std::string path) override;
+
+	Map* GetMap() const;
+
+private:
+	Map* _map;
 };
 
