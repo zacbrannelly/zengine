b3228049d934f98d0902ae7ca4e70b37db769a21
Zac Brannelly <101668878@student.swin.edu.au>
2018-12-20 03:55:27 +1100

    Implement multi-line text field abstraction (inside GUITextField)

--- ZEngine-3.0/GUITextField.cpp
+++ ZEngine-3.0/GUITextField.cpp
@@ -6,6 +6,8 @@ GUITextField::GUITextField(std::string label)
 	_label = label;
 	_buffer = new char[GUITEXTFIELD_BUFFER_SIZE];
 	_bufferSize = GUITEXTFIELD_BUFFER_SIZE;
+	_isMultiline = false;
+	_size = ImVec2(0, 0);
 
 	memset(_buffer, '\0', _bufferSize);
 }
@@ -31,9 +33,60 @@ std::string GUITextField::GetText() const
 	return std::string(_buffer);
 }
 
+void GUITextField::SetBufferSize(int size)
+{
+	if (size != _bufferSize)
+	{
+		delete[] _buffer;
+
+		_buffer = new char[size];
+		memset(_buffer, '\0', size);
+	}
+
+	_bufferSize = size;
+}
+
+int GUITextField::GetButterSize() const
+{
+	return _bufferSize;
+}
+
+void GUITextField::SetMultiline(bool isMulti)
+{
+	_isMultiline = isMulti;
+
+	if (_isMultiline &&_bufferSize < 1000000)
+	{
+		SetBufferSize(1000000);
+	}
+}
+
+void GUITextField::SetSize(float width, float height)
+{
+	_size = { width, height };
+}
+
+float GUITextField::GetWidth() const
+{
+	return _size.x;
+}
+
+float GUITextField::GetHeight() const
+{
+	return _size.y;
+}
+
+bool GUITextField::IsMultiline() const
+{
+	return _isMultiline;
+}
+
 void GUITextField::RenderElement()
 {
-	ImGui::InputText(_label.c_str(), _buffer, _bufferSize);
+	if (!_isMultiline)
+		ImGui::InputText(_label.c_str(), _buffer, _bufferSize);
+	else
+		ImGui::InputTextMultiline(_label.c_str(), _buffer, _bufferSize, _size, ImGuiInputTextFlags_AllowTabInput);
 }
 
 GUIElementType GUITextField::GetType()
--- ZEngine-3.0/GUITextField.h
+++ ZEngine-3.0/GUITextField.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include "GUIElement.h"
+#include "imgui-includes.h"
 
 #define GUITEXTFIELD_BUFFER_SIZE 1000
 
@@ -17,6 +18,16 @@ public:
 	void SetText(std::string text);
 	std::string GetText() const;
 
+	void SetBufferSize(int size);
+	int GetButterSize() const;
+
+	void SetMultiline(bool isMulti);
+	bool IsMultiline() const;
+
+	void SetSize(float width, float height);
+	float GetWidth() const;
+	float GetHeight() const;
+
 	void RenderElement() override;
 	GUIElementType GetType();
 
@@ -24,4 +35,6 @@ private:
 	std::string _label;
 	char* _buffer;
 	int _bufferSize;
+	bool _isMultiline;
+	ImVec2 _size;
 };
