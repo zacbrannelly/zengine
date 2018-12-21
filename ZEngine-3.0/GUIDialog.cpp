ab3e7c096bec26fde0716439144e6ec83862c560
Zac Brannelly <101668878@student.swin.edu.au>
2018-12-21 17:32:46 +1100

    Implement GUIDialog for popup & modal windows

---
+++ ZEngine-3.0/GUIDialog.cpp
@@ -0,0 +1,100 @@
+#include "GUIDialog.h"
+#include "GUINameGenerator.h"
+
+GUIDialog::GUIDialog(std::string name, int width, int height, bool isModal) : GUIWindow(GUINameGenerator::GetUniqueName(name), width, height, false)
+{
+	_isModal = isModal;
+	_result = DIALOG_RESULT_NONE;
+}
+
+void GUIDialog::Show()
+{
+	if (!ImGui::IsPopupOpen(GetTitle().c_str()))
+	{
+		ImGui::OpenPopup(GetTitle().c_str());
+	}
+
+	_isVisible = true;
+}
+
+void GUIDialog::Hide()
+{
+	if (ImGui::IsPopupOpen(GetTitle().c_str()))
+	{
+		ImGui::CloseCurrentPopup();
+	}
+
+	_result = DIALOG_RESULT_NONE;
+	_isVisible = false;
+}
+
+void GUIDialog::Close()
+{
+	if (IsVisible())
+		ImGui::CloseCurrentPopup();
+
+	SetCloseRequested(true);
+}
+
+void GUIDialog::RenderElement()
+{
+	auto visible = ImGui::IsPopupOpen(GetTitle().c_str());
+
+	if (visible && !_isVisible)
+	{
+		Hide();
+	}
+	else if (_isVisible && !visible)
+	{
+		Show();
+	}
+
+	ImGui::SetNextWindowSize(ImVec2(GetWidth(), GetHeight()));
+
+	bool open = true;
+	bool begin = !_isModal ? ImGui::BeginPopup(GetTitle().c_str(), GetFlags()) : ImGui::BeginPopupModal(GetTitle().c_str(), &open, GetFlags());
+
+	if (begin)
+	{
+		Container::RenderElement();
+		RenderInWindow();
+		ProcessInput();
+
+		ImGui::EndPopup();
+	}
+
+	if (!open && AllowClose())
+	{
+		SetResult(DIALOG_RESULT_CLOSE);
+	}
+}
+
+void GUIDialog::SetResult(GUIDialogResult result)
+{
+	_result = result;
+}
+
+GUIDialogResult GUIDialog::GetResult() const
+{
+	return _result;
+}
+
+void GUIDialog::SetModal(bool isModal)
+{
+	_isModal = isModal;
+}
+
+bool GUIDialog::IsModal() const
+{
+	return _isModal;
+}
+
+bool GUIDialog::IsVisible() const
+{
+	return _isVisible;
+}
+
+GUIDialog::~GUIDialog()
+{
+
+}
---
+++ ZEngine-3.0/GUIDialog.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "GUIWindow.h"
+
+enum GUIDialogResult
+{
+	DIALOG_RESULT_NONE,
+	DIALOG_RESULT_OK,
+	DIALOG_RESULT_NO,
+	DIALOG_RESULT_CLOSE
+};
+
+class GUIDialog : public GUIWindow
+{
+public:
+	GUIDialog(std::string title, int width, int height, bool modal);
+	~GUIDialog();
+
+	void SetModal(bool isModal);
+	bool IsModal() const;
+	bool IsVisible() const;
+
+	virtual void Show();
+	virtual void Hide();
+	virtual void Close();
+
+	virtual void RenderElement() override;
+
+	virtual void SetResult(GUIDialogResult result);
+	virtual GUIDialogResult GetResult() const;
+
+private:
+	bool _isModal;
+	bool _isVisible;
+	GUIDialogResult _result;
+};
