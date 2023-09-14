96e864bb1fa377d7a09a342eb6fb36721ae1bd9d
Zac Brannelly <zac.brannelly@gmail.com>
2023-09-14 21:23:17 +1000

    Rename button labels and add icons to play controls

--- ZEngine-Editor/Windows/EditorToolbar.cpp
+++ ZEngine-Editor/Windows/EditorToolbar.cpp
@@ -28,10 +28,11 @@ void EditorToolbar::RenderElement()
 void EditorToolbar::RenderPlayButton()
 {
   auto windowWidth = ImGui::GetWindowContentRegionWidth();
-  ImVec2 buttonSize(ImGui::CalcTextSize("Build & Run").x + 20, 0);
+  auto playButtonLabel = ICON_FA_PLAY_CIRCLE "  Play";
+  ImVec2 buttonSize(ImGui::CalcTextSize(playButtonLabel).x + 20, 0);
 
   ImGui::SetCursorPosX(windowWidth / 2 - buttonSize.x / 2);
-  if (ImGui::Button("Build & Run", buttonSize)) {
+  if (ImGui::Button(playButtonLabel, buttonSize)) {
     _mapController->Play();
   }
 }
@@ -40,10 +41,11 @@ void EditorToolbar::RenderPauseAndStopButtons()
 {
   auto windowWidth = ImGui::GetWindowContentRegionWidth();
   auto isPaused = _mapController->GetPlayState() == PAUSED;
-  auto pauseToggleLabel = isPaused ? "Continue" : "Pause";
+  auto pauseToggleLabel = isPaused ? (ICON_FA_PLAY_CIRCLE "  Continue") : (ICON_FA_PAUSE_CIRCLE "  Pause");
+  auto stopButtonLabel = ICON_FA_STOP "  Stop";
 
   ImVec2 pauseButtonSize(ImGui::CalcTextSize(pauseToggleLabel).x + 20, 0);
-  ImVec2 stopButtonSize(ImGui::CalcTextSize("Stop").x + 20, 0);
+  ImVec2 stopButtonSize(ImGui::CalcTextSize(stopButtonLabel).x + 20, 0);
 
   ImGui::SetCursorPosX(windowWidth / 2 - pauseButtonSize.x - 5);
   if (ImGui::Button(pauseToggleLabel, pauseButtonSize)) {
@@ -55,7 +57,7 @@ void EditorToolbar::RenderPauseAndStopButtons()
   ImGui::SameLine();
   
   ImGui::SetCursorPosX(windowWidth / 2 + 5);
-  if (ImGui::Button("Stop", stopButtonSize)) {
+  if (ImGui::Button(stopButtonLabel, stopButtonSize)) {
     _mapController->Stop();
   }
 }
