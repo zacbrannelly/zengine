77b82e49454dff493b9f43bb8726af13c6788f63
Zac Brannelly <zac.brannelly@gmail.com>
2024-02-07 00:33:38 +1100

    Remove some more redundant code

--- ZEngine-Core/Component/Importers/JsonImporter.h
+++ ZEngine-Core/Component/Importers/JsonImporter.h
@@ -8,7 +8,13 @@ class JsonImporter : public ComponentImporter<JsonImporter<T>>
 public:
 	ZObject* ImportImpl(std::string name, nlohmann::json& values) override
 	{
+    // Create an instance of the object.
 		auto instance =  Factory::CreateInstance<T>(name, _type);
+    
+    // Inject the values from the json into the instance.
+    // This is done via functions defined by the JSON_SCHEMA_BEGIN/JSON_SCHEMA_END macros in
+    // the corresponding header file for the class.
+    // See Utilities/JsonHelpers.h for more information.
     from_json(values, *instance);
 
     return instance;
